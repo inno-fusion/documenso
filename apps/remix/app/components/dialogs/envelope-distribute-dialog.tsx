02f9690f7466d64e8f2d06c4fd1f110c72699a4e
kaumudpa <kaumud@cueclad.com>
2025-12-18 03:17:10 +0530

    Changed apps/remix/app, apps/remix/public folders & Corrected Branding Logo Files

--- apps/remix/app/components/dialogs/envelope-distribute-dialog.tsx
+++ apps/remix/app/components/dialogs/envelope-distribute-dialog.tsx
@@ -330,7 +330,7 @@ export const EnvelopeDistributeDialog = ({
                                             </SelectItem>
                                           ))}
 
-                                          <SelectItem value={'-1'}>Documenso</SelectItem>
+                                          <SelectItem value={'-1'}>0xDocHub</SelectItem>
                                         </SelectContent>
                                       </Select>
                                     </FormControl>
--- apps/remix/app/components/general/envelope-editor/envelope-editor-settings-dialog.tsx
+++ apps/remix/app/components/general/envelope-editor/envelope-editor-settings-dialog.tsx
@@ -753,7 +753,7 @@ export const EnvelopeEditorSettingsDialog = ({
                                           </SelectItem>
                                         ))}
 
-                                        <SelectItem value={'-1'}>Documenso</SelectItem>
+                                        <SelectItem value={'-1'}>0xDocHub</SelectItem>
                                       </SelectContent>
                                     </Select>
                                   </FormControl>
